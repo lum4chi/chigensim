e128375612f548069e65cd8fc27059a299a84aad
lum4chi <lum4chi@gmail.com>
2016-06-16 14:37:56 +0200

    Gather statistics while n-grammin

--- models/corpus2ngrams.py
+++ models/corpus2ngrams.py
@@ -2,34 +2,45 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2016 Francesco Lumachi <francesco.lumachi@gmail.com>
-from gensim import interfaces, utils
+from gensim import interfaces, utils, corpora
 from itertools import tee, izip, islice, chain
 
 
 class Corpus2Ngrams(interfaces.TransformationABC):
     """
         Read a corpus, seen as a list of tokens, and return them as
-        a N-tuple of tokens.
+        a N-tuple of tokens. If required, can use a gensim Dictionary to
+        count statistics on corpus before "n-gramming", useful to compute
+        probabilities.
     """
-    def __init__(self, n, start_char="^", end_char="$", emit_padding=True):
+    def __init__(self, n, start_char="^", end_char="$", emit_padding=True,
+                 dictionary=None):
         """
             Configure returned ngram
         :param n: # of token per -gram
         :param start_char: special char for doc start
         :param end_char: special char for doc end
         :param emit_padding: emit n-gram with start/end chars
+        :param dictionary: can grab statistics before trasformation process
         """
         assert n > 0    # 0-gram not allowed
         self.n = n
         self.start_char, self.end_char = start_char, end_char
         self.emit_padding = emit_padding
+        self.tokens = corpora.Dictionary() if dictionary is None else dictionary
 
-    def __getitem__(self, doc):
-        # if doc is an iterable apply to all
-        is_corpus, doc = utils.is_corpus(doc)
+    def __getitem__(self, corpus_or_doc):
+        # if doc is a corpus, apply to all
+        is_corpus, corpus_or_doc = utils.is_corpus(corpus_or_doc)
         if is_corpus:
-            return self._apply(doc)
+            corpus = corpus_or_doc
+            return self._apply(corpus)
 
+        doc = corpus_or_doc
+        # --- Saving statistics on plain doc
+        self.tokens.doc2bow(doc, allow_update=True)
+
+        # --- Transform doc as a list of n-grams (n-tuples)
         # introduce start/end chars to "n-grammize" head and tail of a doc
         doc = chain([self.start_char] * (self.n-1),
                     doc,
